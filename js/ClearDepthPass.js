b535e9e61cbd7d8d699bde14bab465d7ab1acda0
Steven Kimpe <info@tothedot.be>
2024-11-13 21:37:38 +0100

    added ClearDepthPass

---
+++ js/ClearDepthPass.js
@@ -0,0 +1,18 @@
+
+import { Pass } from 'three/addons/postprocessing/Pass.js';
+
+class ClearDepthPass extends Pass 
+{
+    constructor() 
+    {
+		  super();
+	  }
+
+    render(renderer, writeBuffer, readBuffer /*, deltaTime, maskActive */) 
+    {
+		  renderer.setRenderTarget( this.renderToScreen ? null : readBuffer );
+      renderer.clearDepth();
+	  }
+}
+
+export { ClearDepthPass };
