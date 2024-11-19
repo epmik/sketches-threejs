00bbb8885fe31f3727a9cdbba1be88d434d77d2e
epmik <steven.kimpe@tothedot.be>
2024-11-19 12:30:14 +0100

    VisualizeNoiseShader development

--- 00000000-005-custom-postprocessing-003/.shaders/VisualizeNoiseShader.js
+++ 00000000-005-custom-postprocessing-003/.shaders/VisualizeNoiseShader.js
@@ -260,6 +260,172 @@ const VisualizeNoiseShader = {
 
 		// --------------------------------
 
+		//
+		// GLSL textureless classic 2D noise "cnoise",
+		// with an RSL-style periodic variant "pnoise".
+		// Author:  Stefan Gustavson (stefan.gustavson@liu.se)
+		// Version: 2024-11-07
+		//
+		// Many thanks to Ian McEwan of Ashima Arts for the
+		// ideas for permutation and gradient selection.
+		//
+		// Copyright (c) 2011 Stefan Gustavson. All rights reserved.
+		// Distributed under the MIT license. See LICENSE file.
+		// https://github.com/stegu/webgl-noise
+		//
+
+		vec4 mod289(vec4 x)
+		{
+		  return x - floor(x * (1.0 / 289.0)) * 289.0;
+		}
+		
+		vec4 permute(vec4 x)
+		{
+		  return mod289(((x*34.0)+10.0)*x);
+		}
+
+		vec4 taylorInvSqrt(vec4 r)
+		{
+		return 1.79284291400159 - 0.85373472095314 * r;
+		}
+
+		vec2 fade(vec2 t) {
+		return t*t*t*(t*(t*6.0-15.0)+10.0);
+		}
+
+		// Classic Perlin noise
+		float cnoise(vec2 P)
+		{
+		vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+		vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+		Pi = mod289(Pi); // To avoid truncation effects in permutation
+		vec4 ix = Pi.xzxz;
+		vec4 iy = Pi.yyww;
+		vec4 fx = Pf.xzxz;
+		vec4 fy = Pf.yyww;
+
+		vec4 i = permute(permute(ix) + iy);
+
+		vec4 gx = fract(i * (1.0 / 41.0)) * 2.0 - 1.0 ;
+		vec4 gy = abs(gx) - 0.5 ;
+		vec4 tx = floor(gx + 0.5);
+		gx = gx - tx;
+
+		vec2 g00 = vec2(gx.x,gy.x);
+		vec2 g10 = vec2(gx.y,gy.y);
+		vec2 g01 = vec2(gx.z,gy.z);
+		vec2 g11 = vec2(gx.w,gy.w);
+
+		vec4 norm = taylorInvSqrt(vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11)));
+
+		float n00 = norm.x * dot(g00, vec2(fx.x, fy.x));
+		float n10 = norm.y * dot(g10, vec2(fx.y, fy.y));
+		float n01 = norm.z * dot(g01, vec2(fx.z, fy.z));
+		float n11 = norm.w * dot(g11, vec2(fx.w, fy.w));
+
+		vec2 fade_xy = fade(Pf.xy);
+		vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+		float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+		return 2.3 * n_xy;
+		}
+
+		// Classic Perlin noise, periodic variant
+		float pnoise(vec2 P, vec2 rep)
+		{
+		vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+		vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+		Pi = mod(Pi, rep.xyxy); // To create noise with explicit period
+		Pi = mod289(Pi);        // To avoid truncation effects in permutation
+		vec4 ix = Pi.xzxz;
+		vec4 iy = Pi.yyww;
+		vec4 fx = Pf.xzxz;
+		vec4 fy = Pf.yyww;
+
+		vec4 i = permute(permute(ix) + iy);
+
+		vec4 gx = fract(i * (1.0 / 41.0)) * 2.0 - 1.0 ;
+		vec4 gy = abs(gx) - 0.5 ;
+		vec4 tx = floor(gx + 0.5);
+		gx = gx - tx;
+
+		vec2 g00 = vec2(gx.x,gy.x);
+		vec2 g10 = vec2(gx.y,gy.y);
+		vec2 g01 = vec2(gx.z,gy.z);
+		vec2 g11 = vec2(gx.w,gy.w);
+
+		vec4 norm = taylorInvSqrt(vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11)));
+
+		float n00 = norm.x * dot(g00, vec2(fx.x, fy.x));
+		float n10 = norm.y * dot(g10, vec2(fx.y, fy.y));
+		float n01 = norm.z * dot(g01, vec2(fx.z, fy.z));
+		float n11 = norm.w * dot(g11, vec2(fx.w, fy.w));
+
+		vec2 fade_xy = fade(Pf.xy);
+		vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+		float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+		return 2.3 * n_xy;
+		}		
+
+		// --------------------------------
+
+		// #version 120
+
+		// Cellular noise ("Worley noise") in 2D in GLSL.
+		// Copyright (c) Stefan Gustavson 2011-04-19. All rights reserved.
+		// This code is released under the conditions of the MIT license.
+		// See LICENSE file for details.
+		// https://github.com/stegu/webgl-noise
+
+		// Modulo 7 without a division
+		vec3 mod7(vec3 x) {
+		return x - floor(x * (1.0 / 7.0)) * 7.0;
+		}
+
+		// Cellular noise, returning F1 and F2 in a vec2.
+		// Standard 3x3 search window for good F1 and F2 values
+		vec2 cellular(vec2 P) {
+		#define K 0.142857142857 // 1/7
+		#define Ko 0.428571428571 // 3/7
+		#define jitter 1.0 // Less gives more regular pattern
+			vec2 Pi = mod289(floor(P));
+			vec2 Pf = fract(P);
+			vec3 oi = vec3(-1.0, 0.0, 1.0);
+			vec3 of = vec3(-0.5, 0.5, 1.5);
+			vec3 px = permute(Pi.x + oi);
+			vec3 p = permute(px.x + Pi.y + oi); // p11, p12, p13
+			vec3 ox = fract(p*K) - Ko;
+			vec3 oy = mod7(floor(p*K))*K - Ko;
+			vec3 dx = Pf.x + 0.5 + jitter*ox;
+			vec3 dy = Pf.y - of + jitter*oy;
+			vec3 d1 = dx * dx + dy * dy; // d11, d12 and d13, squared
+			p = permute(px.y + Pi.y + oi); // p21, p22, p23
+			ox = fract(p*K) - Ko;
+			oy = mod7(floor(p*K))*K - Ko;
+			dx = Pf.x - 0.5 + jitter*ox;
+			dy = Pf.y - of + jitter*oy;
+			vec3 d2 = dx * dx + dy * dy; // d21, d22 and d23, squared
+			p = permute(px.z + Pi.y + oi); // p31, p32, p33
+			ox = fract(p*K) - Ko;
+			oy = mod7(floor(p*K))*K - Ko;
+			dx = Pf.x - 1.5 + jitter*ox;
+			dy = Pf.y - of + jitter*oy;
+			vec3 d3 = dx * dx + dy * dy; // d31, d32 and d33, squared
+			// Sort out the two smallest distances (F1, F2)
+			vec3 d1a = min(d1, d2);
+			d2 = max(d1, d2); // Swap to keep candidates for F2
+			d2 = min(d2, d3); // neither F1 nor F2 are now in d3
+			d1 = min(d1a, d2); // F1 is now in d1
+			d2 = max(d1a, d2); // Swap to keep candidates for F2
+			d1.xy = (d1.x < d1.y) ? d1.xy : d1.yx; // Swap if smaller
+			d1.xz = (d1.x < d1.z) ? d1.xz : d1.zx; // F1 is in d1.x
+			d1.yz = min(d1.yz, d2.yz); // F2 is now not in d2.yz
+			d1.y = min(d1.y, d1.z); // nor in  d1.z
+			d1.y = min(d1.y, d2.x); // F2 is in d1.y, we're done.
+			return sqrt(d1.xy);
+		}		
+
+		// --------------------------------
+
 		void main()
 		{
 			// for an explanation about gl_FragCoord, see https://computergraphics.stackexchange.com/a/5725
@@ -276,19 +442,19 @@ const VisualizeNoiseShader = {
 					b = (1.0 + AndrewMeservyKenPerlinNoise((gl_FragCoord.xyz + vec3(9.2487, 1.89832, 4.3289)) * multiplier)) * 0.5;
 					break;
 				case 2:
-					r = gold_noise(gl_FragCoord.xy, 0.0);
-					g = gold_noise(gl_FragCoord.xy, 0.2487);
-					b = gold_noise(gl_FragCoord.xy, 0.3289);
-					break;
-				case 3:
-					r = randomImprovedOneLiner(gl_FragCoord.xy);
-					g = randomImprovedOneLiner(gl_FragCoord.xy + vec2(4.3289, 9.2487));
-					b = randomImprovedOneLiner(gl_FragCoord.xy + vec2(9.2487, 4.3289));
-					break;
-				default:
 					r = (1.0 + snoise(gl_FragCoord.xy * multiplier.xy)) * 0.5;
 					g = (1.0 + snoise((gl_FragCoord.xy + vec2(4.3289, 9.2487)) * multiplier.xy)) * 0.5;
 					b = (1.0 + snoise((gl_FragCoord.xy + vec2(9.2487, 1.89832)) * multiplier.xy)) * 0.5;
+					break;
+				case 3:
+					r = (1.0 + cnoise(gl_FragCoord.xy * multiplier.xy)) * 0.5;
+					g = (1.0 + cnoise((gl_FragCoord.xy + vec2(4.3289, 9.2487)) * multiplier.xy)) * 0.5;
+					b = (1.0 + cnoise((gl_FragCoord.xy + vec2(9.2487, 1.89832)) * multiplier.xy)) * 0.5;
+					break;
+				default:
+					r = (1.0 + cellular(gl_FragCoord.xy * multiplier.xy)) * 0.5;
+					g = (1.0 + cellular((gl_FragCoord.xy + vec2(4.3289, 9.2487)) * multiplier.xy)) * 0.5;
+					b = (1.0 + cellular((gl_FragCoord.xy + vec2(9.2487, 1.89832)) * multiplier.xy)) * 0.5;
 					break;
 			}			
 
