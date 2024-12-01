7012a9002e3bd72d6fb9c715760f88247067c0e8
epmik <steven.kimpe@tothedot.be>
2024-12-01 08:10:40 +0100

    shadow and path-tracer try-outs

--- 20241130-004-shadow-gpu-path-tracer/.postprocessing/Export.module.js
+++
@@ -1,6 +0,0 @@
-export { NoisePixelDisplacePass } from '../.postprocessing/NoisePixelDisplacePass.js';
-export { RandomPixelStretchPass } from '../.postprocessing/RandomPixelStretchPass.js';
-export { MondriaanLinesPass } from '../.postprocessing/MondriaanLinesPass.js';
-export { UvVisualizerPass } from '../.postprocessing/UvVisualizerPass.js';
-export { RandomPixelDisplacePass } from '../.postprocessing/RandomPixelDisplacePass.js';
-export { DotScreenPass } from '../.postprocessing/DotScreenPass.js';
