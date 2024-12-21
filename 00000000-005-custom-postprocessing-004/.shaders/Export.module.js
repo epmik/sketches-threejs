cb61e98a1fd7a23c4b8e3e6832f4b6af52c0dcf8
epmik <steven.kimpe@tothedot.be>
2024-12-21 18:53:52 +0100

    added 00000000-005-custom-postprocessing-004

---
+++ 00000000-005-custom-postprocessing-004/.shaders/Export.module.js
@@ -0,0 +1,28 @@
+export { NoisePixelDisplaceShader } from '../.shaders/NoisePixelDisplaceShader.js';
+export { RandomPixelStretchShader } from '../.shaders/RandomPixelStretchShader.js';
+export { MondriaanLinesShader } from '../.shaders/MondriaanLinesShader.js';
+export { UvVisualizerShader } from '../.shaders/UvVisualizerShader.js';
+export { RandomPixelDisplaceShader } from '../.shaders/RandomPixelDisplaceShader.js';
+export { VisualizeRandomShader } from '../.shaders/VisualizeRandomShader.js';
+export { VisualizeNoiseShader } from '../.shaders/VisualizeNoiseShader.js';
+export { OvercastSkyShader } from '../.shaders/OvercastSkyShader.js';
+export { ColorAdjustShader } from '../.shaders/ColorAdjustShader.js';
+export { GradientShader } from '../.shaders/GradientShader.js';
+export { GradientShader2 } from '../.shaders/GradientShader2.js';
+
+import { ShaderChunk } from 'three';
+
+import mod289_glsl from '../.shaders/mod289.glsl.js';
+import permute_glsl from '../.shaders/permute.glsl.js';
+import taylorInvSqrt_glsl from '../.shaders/taylorInvSqrt.glsl.js';
+import noise2D_glsl from '../.shaders/noise2D.glsl.js';
+import noise3D_glsl from '../.shaders/noise3D.glsl.js';
+import noise4D_glsl from '../.shaders/noise4D.glsl.js';
+
+ShaderChunk['mod289_glsl'] = mod289_glsl;
+ShaderChunk['permute_glsl'] = permute_glsl;
+ShaderChunk['taylorInvSqrt_glsl'] = taylorInvSqrt_glsl;
+ShaderChunk['noise2D_glsl'] = noise2D_glsl;
+ShaderChunk['noise3D_glsl'] = noise3D_glsl;
+ShaderChunk['noise4D_glsl'] = noise4D_glsl;
+
