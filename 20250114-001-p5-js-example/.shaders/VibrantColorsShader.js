3eb3800e181a746d537e68d6f58ece49e65c8ce2
epmik <steven.kimpe@tothedot.be>
2025-01-15 21:50:55 +0100

    development

--- 20250114-001-p5-js-example/.shaders/VibrantColorsShader.js
+++ 20250114-001-p5-js-example/.shaders/VibrantColorsShader.js
@@ -81,20 +81,26 @@ const VibrantColorsShader = {
 	},
 
 	vertexShader: /* glsl */`
+		
+		uniform mat4 uModelViewMatrix;
+		uniform mat4 uProjectionMatrix;
+
+		attribute vec3 aPosition;
+		attribute vec2 aTexCoord;
 
 		varying vec2 vUv;
 
 		void main() {
 
-			vUv = uv;
+			vUv = aTexCoord;
 
-			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+			gl_Position = uProjectionMatrix * uModelViewMatrix * vec4( aPosition, 1.0 );
 
 		}`,
 
 	fragmentShader: /* glsl */`
 
-		#include <common>
+		// #include <common>
 
 		uniform float seed;
 		uniform float coverPercentage;
@@ -109,101 +115,105 @@ const VibrantColorsShader = {
 		uniform float maxContrast;
 
 		varying vec2 vUv;
-		// A single iteration of Bob Jenkins' One-At-A-Time hashing algorithm.
-		uint hash( uint x ) {
-			x += ( x << 10u );
-			x ^= ( x >>  6u );
-			x += ( x <<  3u );
-			x ^= ( x >> 11u );
-			x += ( x << 15u );
-			return x;
-		}
 
-		// Compound versions of the hashing algorithm I whipped together.
-		uint hash( uvec2 v ) { return hash( v.x ^ hash(v.y)                         ); }
-		uint hash( uvec3 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z)             ); }
-		uint hash( uvec4 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z) ^ hash(v.w) ); }
+		// // A single iteration of Bob Jenkins' One-At-A-Time hashing algorithm.
+		// uint hash( uint x ) {
+		// 	x += ( x << 10u );
+		// 	x ^= ( x >>  6u );
+		// 	x += ( x <<  3u );
+		// 	x ^= ( x >> 11u );
+		// 	x += ( x << 15u );
+		// 	return x;
+		// }
 
-		// Construct a float with half-open range [0:1] using low 23 bits.
-		// All zeroes yields 0.0, all ones yields the next smallest representable value below 1.0.
-		float floatConstruct( uint m ) {
-			const uint ieeeMantissa = 0x007FFFFFu; // binary32 mantissa bitmask
-			const uint ieeeOne      = 0x3F800000u; // 1.0 in IEEE binary32
+		// // Compound versions of the hashing algorithm I whipped together.
+		// uint hash( uvec2 v ) { return hash( v.x ^ hash(v.y)                         ); }
+		// uint hash( uvec3 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z)             ); }
+		// uint hash( uvec4 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z) ^ hash(v.w) ); }
 
-			m &= ieeeMantissa;                     // Keep only mantissa bits (fractional part)
-			m |= ieeeOne;                          // Add fractional part to 1.0
+		// // Construct a float with half-open range [0:1] using low 23 bits.
+		// // All zeroes yields 0.0, all ones yields the next smallest representable value below 1.0.
+		// float floatConstruct( uint m ) {
+		// 	const uint ieeeMantissa = 0x007FFFFFu; // binary32 mantissa bitmask
+		// 	const uint ieeeOne      = 0x3F800000u; // 1.0 in IEEE binary32
 
-			float  f = uintBitsToFloat( m );       // Range [1:2]
-			return f - 1.0;                        // Range [0:1]
-		}
+		// 	m &= ieeeMantissa;                     // Keep only mantissa bits (fractional part)
+		// 	m |= ieeeOne;                          // Add fractional part to 1.0
 
-		// Pseudo-random value in half-open range [0:1].
-		float random( float x ) { return floatConstruct(hash(floatBitsToUint(x))); }
-		float random( vec2  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
-		float random( vec3  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
-		float random( vec4  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+		// 	float  f = uintBitsToFloat( m );       // Range [1:2]
+		// 	return f - 1.0;                        // Range [0:1]
+		// }
 
-		float random( float x, float min, float max ) { return min + (max * random(x)); }
-		float random( vec2 v, float min, float max ) { return min + (max * random(v)); }
-		float random( vec3 v, float min, float max ) { return min + (max * random(v)); }
-		float random( vec4 v, float min, float max ) { return min + (max * random(v)); }
+		// // Pseudo-random value in half-open range [0:1].
+		// float random( float x ) { return floatConstruct(hash(floatBitsToUint(x))); }
+		// float random( vec2  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+		// float random( vec3  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+		// float random( vec4  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+
+		// float random( float x, float min, float max ) { return min + (max * random(x)); }
+		// float random( vec2 v, float min, float max ) { return min + (max * random(v)); }
+		// float random( vec3 v, float min, float max ) { return min + (max * random(v)); }
+		// float random( vec4 v, float min, float max ) { return min + (max * random(v)); }
 
 		void main()
 		{
 			gl_FragColor = texture2D( tDiffuse, vUv );
 
-			bool specle = coverPercentage < 1.0
-				? coverPercentage > 0.0
-					? random( vec3(gl_FragCoord.xy, seed)) <= coverPercentage
-					: false
-				: true;
+			// bool specle = coverPercentage < 1.0
+			// 	? coverPercentage > 0.0
+			// 		? random( vec3(gl_FragCoord.xy, seed)) <= coverPercentage
+			// 		: false
+			// 	: true;
 
-			if(specle)
-			{
-				float hue = random(vec3(gl_FragCoord.xy, seed), minHue, maxHue);
+			// if(specle)
+			// {
+			// 	float hue = random(vec3(gl_FragCoord.xy, seed), minHue, maxHue);
 			
-				if(hue != 0.0)
-				{
-					// hue
-					float angle = hue * 3.14159265;
-					float s = sin(angle), c = cos(angle);
-					vec3 weights = (vec3(2.0 * c, -sqrt(3.0) * s - c, sqrt(3.0) * s - c) + 1.0) / 3.0;
-					float len = length(gl_FragColor.rgb);
-					gl_FragColor.rgb = vec3(
-						dot(gl_FragColor.rgb, weights.xyz),
-						dot(gl_FragColor.rgb, weights.zxy),
-						dot(gl_FragColor.rgb, weights.yzx)
-					);
-				}
+			// 	if(hue != 0.0)
+			// 	{
+			// 		// hue
+			// 		float angle = hue * 3.14159265;
+			// 		float s = sin(angle), c = cos(angle);
+			// 		vec3 weights = (vec3(2.0 * c, -sqrt(3.0) * s - c, sqrt(3.0) * s - c) + 1.0) / 3.0;
+			// 		float len = length(gl_FragColor.rgb);
+			// 		gl_FragColor.rgb = vec3(
+			// 			dot(gl_FragColor.rgb, weights.xyz),
+			// 			dot(gl_FragColor.rgb, weights.zxy),
+			// 			dot(gl_FragColor.rgb, weights.yzx)
+			// 		);
+			// 	}
 
-				float saturation = random(vec3(gl_FragCoord.xy, seed) + vec3(0.2871, 4.8954, 3.4598), minSaturation, maxSaturation);
+			// 	float saturation = random(vec3(gl_FragCoord.xy, seed) + vec3(0.2871, 4.8954, 3.4598), minSaturation, maxSaturation);
 
-				if(saturation != 0.0)
-				{
-					// saturation
-					float average = (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b) / 3.0;
-					if (saturation > 0.0) {
-						gl_FragColor.rgb += (average - gl_FragColor.rgb) * (1.0 - 1.0 / (1.001 - saturation));
-					} else {
-						gl_FragColor.rgb += (average - gl_FragColor.rgb) * (-saturation);
-					}
-				}
+			// 	if(saturation != 0.0)
+			// 	{
+			// 		// saturation
+			// 		float average = (gl_FragColor.r + gl_FragColor.g + gl_FragColor.b) / 3.0;
+			// 		if (saturation > 0.0) {
+			// 			gl_FragColor.rgb += (average - gl_FragColor.rgb) * (1.0 - 1.0 / (1.001 - saturation));
+			// 		} else {
+			// 			gl_FragColor.rgb += (average - gl_FragColor.rgb) * (-saturation);
+			// 		}
+			// 	}
 
-				float brightness = random(vec3(gl_FragCoord.xy, seed) + vec3(4.8954, 3.4598, 0.2871), minBrightness, maxBrightness);
+			// 	float brightness = random(vec3(gl_FragCoord.xy, seed) + vec3(4.8954, 3.4598, 0.2871), minBrightness, maxBrightness);
 
-				gl_FragColor.rgb += brightness;
+			// 	gl_FragColor.rgb += brightness;
 
-				float contrast = random(vec3(gl_FragCoord.xy, seed) + vec3(3.4598, 0.2871, 4.8954), minContrast, maxContrast);
+			// 	float contrast = random(vec3(gl_FragCoord.xy, seed) + vec3(3.4598, 0.2871, 4.8954), minContrast, maxContrast);
 
-				if (contrast > 0.0) 
-				{
-					gl_FragColor.rgb = (gl_FragColor.rgb - 0.5) / (1.0 - contrast) + 0.5;
-				} 
-				else 
-				{
-					gl_FragColor.rgb = (gl_FragColor.rgb - 0.5) * (1.0 + contrast) + 0.5;
-				}				
-			}
+			// 	if (contrast > 0.0)
+			// 	{
+			// 		gl_FragColor.rgb = (gl_FragColor.rgb - 0.5) / (1.0 - contrast) + 0.5;
+			// 	}
+			// 	else
+			// 	{
+			// 		gl_FragColor.rgb = (gl_FragColor.rgb - 0.5) * (1.0 + contrast) + 0.5;
+			// 	}
+					
+			// }
+			
+			gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 		}`
 
 };
